a1384aff081fa794b5623fe3beeb578fb4a0d64e
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2018-06-04 13:36:11 +0600

    Initial Commit

---
+++ iOSSecondTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSSecondTask
+//
+//  Created by MacBook Pro on 6/4/18.
+//  Copyright © 2018 Islam & Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
